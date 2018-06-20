ab6bb8eecec43cf9a59894694aa9d67f7748f9da
Patrik Ando <patr589d@edu.easj.dk>
2018-06-20 18:22:30 +0200

    ADD : CalendarPage core elements

--- SalaryTrack/View/LandingPage.xaml.cs
+++ SalaryTrack/View/LandingPage.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using SalaryTrack.View;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SalaryTrack
@@ -9,11 +13,41 @@ namespace SalaryTrack
         {
             this.InitializeComponent();
             BasicElementSetup();
+
+            this.InitializeComponent();
         }
 
+        #region Setup Methods
         private void BasicElementSetup()
         {
             LandingFrame.Navigate(typeof(CalendarPage));
+            TitleBarSetup();
         }
+
+        private void TitleBarSetup()
+        {
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+        }
+        #endregion
+
+        #region UI Element Actions
+        private void ProfileBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            LandingFrame.Navigate(typeof(ProfilePage));
+        }
+
+        private void CalendarBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            LandingFrame.Navigate(typeof(CalendarPage));
+        }
+
+        private void OverviewBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            LandingFrame.Navigate(typeof(OverviewPage));
+        }
+        #endregion
     }
 }
